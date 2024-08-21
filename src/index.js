542004285b6ad47f1164a1e1ed2fda9968adf138
NoName2212013 <rajdeepbarman733@gmail.com>
2024-08-22 02:10:11 +0530

    Ready For Deployment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevTools();
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
