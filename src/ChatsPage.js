fd63cf39566d15c60e562bb50c92aefba2412e71
NoName2212013 <rajdeepbarman733@gmail.com>
2024-08-25 14:44:21 +0530

    Chat engine ID changed

--- src/ChatsPage.js
+++ src/ChatsPage.js
@@ -4,7 +4,7 @@ const ChatsPage = (props)=> {
     return (
         <div style={{ height:'100vh'}}>
             <PrettyChatWindow
-                projectId='8bde53a5-01fc-4e06-859f-650fefdfd53f'
+                projectId='44f8e13c-d40e-4ff5-957c-1df4c15fe38f'
                 username={props.user.username}
                 secret={props.user.secret}
                 style={{height: '100%'}}
