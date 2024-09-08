cf8192c12ea5ff2ec9c2a2e5e0962dd460933070
NoName2212013 <rajdeepbarman733@gmail.com>
2024-09-08 21:32:44 +0530

    Product key renewed

--- src/ChatsPage.js
+++ src/ChatsPage.js
@@ -4,7 +4,7 @@ const ChatsPage = (props)=> {
     return (
         <div style={{ height:'100vh'}}>
             <PrettyChatWindow
-                projectId='44f8e13c-d40e-4ff5-957c-1df4c15fe38f'
+                projectId='902ee088-17a8-43a4-b695-767e5b2d0804'
                 username={props.user.username}
                 secret={props.user.secret}
                 style={{height: '100%'}}
