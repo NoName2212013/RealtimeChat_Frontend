134f29e045d46940c56478fd2eca95bea24c7428
NoName2212013 <rajdeepbarman733@gmail.com>
2024-10-02 20:49:38 +0530

    Routine Maintenance

--- src/ChatsPage.js
+++ src/ChatsPage.js
@@ -4,7 +4,7 @@ const ChatsPage = (props)=> {
     return (
         <div style={{ height:'100vh'}}>
             <PrettyChatWindow
-                projectId='902ee088-17a8-43a4-b695-767e5b2d0804'
+                projectId='62862a3a-fc8f-4253-a73a-741be0008210'
                 username={props.user.username}
                 secret={props.user.secret}
                 style={{height: '100%'}}
